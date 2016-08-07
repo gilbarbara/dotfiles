e7b860c494bc0972c01c222e0f516aceda452139
Gil Barbara <gilbarbara@gmail.com>
2016-08-07 19:49:17 -0300

    update service

--- bin/service.sh
+++ bin/service.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # check for empty
-if [ "$#" == 0 ] || ([ "$#" -lt 2 ] || [ "$2" -ne "list" ]); then
-  echo "usage \`service [daemon] [start|stop|restart]\`"
+if [ "$#" -lt 2 ] && [ "$1" != "list" ]; then
+  echo "usage \`service [daemon] [start|stop|restart]\` or \`service list\` to see all the daemons"
   exit 1
 fi
 
-brew services $2 $1
+[[ "$#" -lt 2 ]] && command=$1 || command="${2} ${1}"
+
+brew services $command
